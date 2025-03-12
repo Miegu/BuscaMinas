5ad2aad27eed34eee74c132056d8eca38b5030ca
Nicole <nicoleav@ucm.es>
2025-03-12 11:53:00 +0100

    main arreglado

--- main.cpp
+++ main.cpp
@@ -9,10 +9,10 @@ int main() {
     tJuego juego;
     tListaUndo listaUndo;
     string nombreArchivo;
-    
+
     // Solicitar nombre de archivo con getline para espacios
     cout << "Introduce el nombre del archivo: ";
-    getline(cin, nombreArchivo); 
+    getline(cin, nombreArchivo);
     nombreArchivo += ".txt";
     if (carga_juego(juego, nombreArchivo)) {
         inicializar_listaUndo(listaUndo);
@@ -20,10 +20,57 @@ int main() {
         do {
             mostrar_cabecera();
             mostrar_juego_consola(juego);
-            pedir_pos(fila, columna);
-            juega(juego, fila, columna, lista_pos);
-        } while (!esta_terminado(juego) && !(fila == -1 && columna == -1));
-    } else {
+            tListaPosiciones listaPos;
+            inicializar_listaPosiciones(listaPos);
+            int fila, columna;
+            pedir_pos(fila, columna); // Función del módulo inputOutput
+
+            // Control de operaciones especiales en main
+            if (fila == -1 && columna == -1) {
+                salir = true;
+                cout << "Partida abandonada\n";
+            }
+            else if (fila == -2 && columna == -2) {
+                // Lógica de marcado/desmarcado
+                int x, y;
+                pedir_pos(x, y);
+                if (es_valida(juego.tablero, x, y)) {
+                    marcar_desmarcar(juego, x, y);
+                }
+            }
+            else if (fila == -3 && columna == -3) {
+                // Operación undo
+                if (longitud_listaUndo(listaUndo) > 0) {
+                    aplicar_undo(juego, listaUndo);
+                    juego.estado = ACTIVO;
+                }
+            }
+            else {
+                // Jugada normal
+                EstadoJuego estado = juega(juego, fila, columna, listaUndo);
+                if (estado == PERDIDO || estado == GANADO) {
+                    mostrar_juego_consola(juego);
+                    mostrar_resultado(juego);
+                    cout << "Seguir Jugando? S/N " << endl;
+                    char c;
+                    cin >> c;
+                    if (c == 'n') {
+                        salir = true;
+                    }
+                    else if (c == 's') {
+                        juego.estado = ACTIVO;
+                        cout << "Nueva Partida: " << endl;
+                        inicializar_listaUndo(listaUndo); // Reiniciar histórico
+                        carga_juego(juego, nombreArchivo); // Recargar tablero
+                        salir = false; // Restablecer estado
+                    }
+
+
+                }
+            }
+        } while (!salir && !esta_terminado(juego));
+    }
+    else {
         cout << "Error al cargar el juego." << endl;
     }
 
